a6399bcb7e60dd38649317567e3cd740de41b8cb
vinayakfr <vinayak.sri@yahoo.com>
2025-06-23 01:11:13 +0530

    Initial Commit

---
+++ WeatherClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherClone
+//
+//  Created by Vinayak Srivastava on 23/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherClone/WeatherCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherCloneApp.swift
+//  WeatherClone
+//
+//  Created by Vinayak Srivastava on 23/06/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
